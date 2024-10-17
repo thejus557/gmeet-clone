4be80e054e07d50004a5c1ef5b3b9a1ee74ea418
surya teja <suryateja.kandukuru@techolution.com>
2024-10-17 17:07:21 +0530

    feat: base structure setup

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(3000, () => {
+  console.log("server running at http://localhost:3000");
+});
---
+++ frontend/src/app/socket.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { io, Socket } from 'socket.io-client';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SocketService {
+  socket!: Socket;
+  constructor() {
+    console.log('c');
+    this.socket = io('http://localhost:3000');
+    this.socket.on('connect', function () {
+      console.log('Connected!');
+    });
+  }
+
+  public sendMessage(message: string) {
+    console.log('emit');
+    setTimeout(() => {
+      this.socket.emit('message', message);
+    }, 5000);
+  }
+
+  public onMessage() {
+    return new Observable((observer) => {
+      this.socket.on('message', (message) => {
+        observer.next(message);
+      });
+    });
+  }
+}
